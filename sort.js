cb1b8bf0023d589d07d5ee16c9c7a7e6e5944342
Tal Amit <83287794+tal0311@users.noreply.github.com>
2023-08-10 14:06:48 +0300

    Create sort.js

---
+++ sort.js
@@ -0,0 +1,22 @@
+/ var gSortBy = { rate: -1, name: 1, price: 1 }
+// sortBy(books, gSortBy)
+
+function sortBy(items, sortBy,) {
+    if (!Object.keys(sortBy).length) return
+    Object.keys(sortBy).forEach((key) => {
+        // [rate, name, price]
+        const isInts = ['price', 'rate']
+        isInts.includes(key)
+            ? items.sort((a, b) => (a[key] - b[key]) * sortBy[key])
+            : items.sort((a, b) => a[key].localeCompare(b[key]) * sortBy[key])
+    })
+}
+
+var gSortBy = { key: 'rate', dir: -1 }
+function sortBy(items, sortBy,) {
+    if (!sortBy.key) return
+    const isInts = ['price', 'rate']
+    isInts.includes(key)
+        ? items.sort((a, b) => (a[sortBy.key] - b[sortBy.key]) * sortBy.dir)
+        : items.sort((a, b) => a[sortBy.key].localeCompare(b[sortBy.key]) * sortBy.dir)
+}
